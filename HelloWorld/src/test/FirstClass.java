1b2124a8d2b59d21f5fda0b57faae0bd44cab50d
preethamb <preethamb656@gmail.com>
2018-04-17 06:12:16 -0700

    New Project Added

---
+++ HelloWorld/src/test/FirstClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author user1
+ *
+ */
+public class FirstClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+	}
+
+}
---
+++ HelloWorld/src/test/Second.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author user1
+ *
+ */
+public class Second {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TOsDO Auto-generated method stub
+System.out.println("my second class");
+	}
+
+}
